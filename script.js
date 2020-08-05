c9e770aa61e025108f6a79512853bc662b4e5b71
venusnmj <venusnmj@hotmail.com>
2020-08-06 01:17:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,33 @@ var navSymbols = document.getElementsByClassName("navSym")
     
 
 
+
+    function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('searchEvents');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("suggestedEvents");
+        li = ul.getElementsByTagName('li');
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }
+
+
+
+
+
+
+
 $(document).ready(function(){
 $(".eduSpaceLogo").hide();
 
@@ -67,4 +94,9 @@ $(".eduSpaceLogo").hide();
         }
         
     });
+
+
+
+
 })
+
